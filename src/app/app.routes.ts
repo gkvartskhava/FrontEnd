f9ddfb956c3a4bdaa46934beaeec483f0ab53277
gkvartskhava <gio.kvartskhava@gmail.com>
2025-09-11 19:33:45 +0200

    starting up with commerce web-site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ErrorComponentComponent } from './error-component/error-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path : '', component : HomePageComponent },
+    
+    { path : '**', component : ErrorComponentComponent}
+];
